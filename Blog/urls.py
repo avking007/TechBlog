d72f7e1be1ada5e17bf433e85d37ede4503fe79b
anish <avanvarshney@gmail.com>
2019-08-28 15:58:27 +0530

    new

--- Blog/urls.py
+++ Blog/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
 
+#newchange
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('basic_app.urls')),
